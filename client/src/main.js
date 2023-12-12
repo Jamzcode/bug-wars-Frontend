a053c14d84712f3e0741b7decdeef7418e92f75d
Steve Her <122183092+Sth84478@users.noreply.github.com>
2023-12-11 22:44:43 -0600

    added baseUrl in main.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API
 
 const app = createApp(App)
 

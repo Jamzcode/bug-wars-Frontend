bad2c65aaead258923d2539a5e545aeb9ceac3b4
Steve Her <122183092+Sth84478@users.noreply.github.com>
2023-12-11 22:01:37 -0600

    initial commit

---
+++ client/src/components/LoginComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <h1>Login page</h1>
+    <login />
+  </div>
+</template>
+
+<script>
+import login from '../components/LoginComponent.vue'
+
+export default {
+  components: { login }
+}
+</script>
+
+<style></style>

87461128bd7e3184dab5d7a4d4f8ed9905866df7
Steve Her <122183092+Sth84478@users.noreply.github.com>
2023-12-18 17:59:58 -0600

    Testing front end to try to fetch api endpoints (#3)
    * Merging Dev into Main (#1)
    
    * initial commit
    
    * initial commit
    
    * Delete bug-wars-client directory
    
    * added basic router
    
    * added baseUrl in main.js
    
    * fixed file structure
    
    ---------
    
    Co-authored-by: Steve Her <122183092+Sth84478@users.noreply.github.com>
    
    * testing api endpoints
    
    ---------
    
    Co-authored-by: Josh Hurley <53581330+jrhurleycode@users.noreply.github.com>

---
+++ src/services/users.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_REMOTE_API,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
+})
+
+export default {
+  getUsers() {
+    return api.get(`/users/all`)
+  },
+  getUserById(id) {
+    return api.get(`/users/${id}`)
+  }
+}

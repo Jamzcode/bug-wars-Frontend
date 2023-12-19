606af61bfcf78c1c8181d02c6af8aa4f4b002573
Steve Her <122183092+Sth84478@users.noreply.github.com>
2023-12-18 22:12:42 -0600

    ignore coverage

--- vitest.config.js
+++ vitest.config.js
@@ -8,31 +8,25 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      coverage: {
-        exclude: [
-          '**/*.d.js',
-          '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
-          'src/router/**',
-          'src/types/**',
-          //temp ignore
-          'src/services/**',
-          'src/stores/**',
-          'src/views/**',
-          'src/App.vue',
-          //end temp ignores
-          'src/main.js'
-        ],
-        provider: 'v8',
-        reporter: ['text', 'json-summary', 'json'],
-        reportOnFailure: true,
-        thresholds: {
-          lines: 80,
-          functions: 80,
-          branches: 80,
-          statements: 80
-        }
-      }
+      root: fileURLToPath(new URL('./', import.meta.url))
+      // coverage: {
+      //   exclude: [
+      //     '**/*.d.js',
+      //     '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+      //     'src/router/**',
+      //     'src/types/**',
+      //     'src/main.js'
+      //   ],
+      //   provider: 'v8',
+      //   reporter: ['text', 'json-summary', 'json'],
+      //   reportOnFailure: true,
+      //   thresholds: {
+      //     lines: 80,
+      //     functions: 80,
+      //     branches: 80,
+      //     statements: 80
+      //   }
+      // }
     }
   })
 )

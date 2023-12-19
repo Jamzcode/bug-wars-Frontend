6ecabf2b7bc6758bc0b38428158dcf6650cd17c0
Steve Her <122183092+Sth84478@users.noreply.github.com>
2023-12-18 21:41:03 -0600

    test

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,14 +1,7 @@
 <template>
   <div>My Home</div>
-<<<<<<< HEAD
   <div>Testing changes</div>
 </template>
 
 <script></script>
-=======
-</template>
-<script>
-export default {}
-</script>
->>>>>>> dev
 <style></style>

c34f45f9321e74a53771083601ef89a87ccbe7ce
Steve Her <122183092+Sth84478@users.noreply.github.com>
2024-01-08 14:30:15 -0600

    working on testing validation

--- src/components/RegisterComponent.spec.js
+++ src/components/RegisterComponent.spec.js
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, assert } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
 import RegisterComponent from './RegisterComponent.vue'
 import authService from '@/services/AuthService'
@@ -16,27 +16,50 @@ describe('RegisterComponent', () => {
     }
   })
 
-  it('displays form correctly', () => {
-    const usernameInput = wrapper.vm.$el.querySelector('input#username')
-    const emailInput = wrapper.vm.$el.querySelector('input#email')
-    const passwordInput = wrapper.vm.$el.querySelector('input#password')
-    const confirmPasswordInput = wrapper.vm.$el.querySelector('input#confirmPassword')
-    const submitButton = wrapper.vm.$el.querySelector('button[type="submit"]')
+  it(
+    'displays form correctly',
+    () => {
+      const usernameInput = wrapper.vm.$el.querySelector('input#username')
+      const emailInput = wrapper.vm.$el.querySelector('input#email')
+      const passwordInput = wrapper.vm.$el.querySelector('input#password')
+      const confirmPasswordInput = wrapper.vm.$el.querySelector('input#confirmPassword')
+      const submitButton = wrapper.vm.$el.querySelector('button[type="submit"]')
 
-    expect(usernameInput).not.toBeNull()
+      expect(usernameInput).not.toBeNull()
 
-    expect(usernameInput).not.toBeNull()
-    expect(usernameInput.placeholder).toBe('Username')
+      expect(usernameInput).not.toBeNull()
+      expect(usernameInput.placeholder).toBe('Username')
 
-    expect(emailInput).not.toBeNull()
-    expect(emailInput.placeholder).toBe('Email')
+      expect(emailInput).not.toBeNull()
+      expect(emailInput.placeholder).toBe('Email')
 
-    expect(passwordInput).not.toBeNull()
-    expect(passwordInput.placeholder).toBe('Password')
+      expect(passwordInput).not.toBeNull()
+      expect(passwordInput.placeholder).toBe('Password')
 
-    expect(confirmPasswordInput).not.toBeNull()
-    expect(confirmPasswordInput.placeholder).toBe('Confirm Password')
+      expect(confirmPasswordInput).not.toBeNull()
+      expect(confirmPasswordInput.placeholder).toBe('Confirm Password')
 
-    expect(submitButton).not.toBeNull()
-  })
+      expect(submitButton).not.toBeNull()
+    },
+    it('submits forms with valid data', async () => {
+      authService.register.returns(Promise.resolve({ status: 201 }))
+
+      await wrapper.setData({
+        user: {
+          username: 'test123',
+          email: 'test123@test.com',
+          password: {
+            password: '123456',
+            confirm: '123456'
+          }
+        }
+      })
+
+      await wrapper.find('form').trigger('submit.prevent')
+      await wrapper.vm.$nextTick()
+
+      assert.calledWith(mockRouter.push, { path: '/login' })
+      assert.calledWith(wrapper.vm.success, 'Thank you for registering, please sign in.')
+    })
+  )
 })

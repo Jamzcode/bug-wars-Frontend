1e79fbe712a51c52a6adc96e56e1493fac741e65
Steve Her <122183092+Sth84478@users.noreply.github.com>
2024-01-08 11:16:45 -0600

    Task registration (#9)
    * Testing github actions (#5)
    
    * initial commit
    
    * initial commit
    
    * Delete bug-wars-client directory
    
    * added basic router
    
    * added baseUrl in main.js
    
    * fixed file structure
    
    * test
    
    * test
    
    * test header
    
    * adding dist
    
    * adding dist
    
    * adding dist
    
    * vite.config updated
    
    * test
    
    * adding dist subtree
    
    * adding dist subtree
    
    * test
    
    * test deploy
    
    * testing changes
    
    * testing to fetch api endpoints from backend
    
    * Testing front end to try to fetch api endpoints (#3)
    
    * Merging Dev into Main (#1)
    
    * initial commit
    
    * initial commit
    
    * Delete bug-wars-client directory
    
    * added basic router
    
    * added baseUrl in main.js
    
    * fixed file structure
    
    ---------
    
    Co-authored-by: Steve Her <122183092+Sth84478@users.noreply.github.com>
    
    * testing api endpoints
    
    ---------
    
    Co-authored-by: Josh Hurley <53581330+jrhurleycode@users.noreply.github.com>
    
    * test actions
    
    * test
    
    * test
    
    * test again
    
    * test test
    
    * comment out coverage
    
    * added tests to test the test
    
    * second test
    
    * ignore coverage
    
    * comment out threshholds
    
    * test 3
    
    * test 4
    
    * test 5
    
    * test
    
    * test
    
    * github actions
    
    * Task GitHub actions (#4)
    
    * Merging Dev into Main (#1)
    
    * initial commit
    
    * initial commit
    
    * Delete bug-wars-client directory
    
    * added basic router
    
    * added baseUrl in main.js
    
    * fixed file structure
    
    ---------
    
    Co-authored-by: Steve Her <122183092+Sth84478@users.noreply.github.com>
    
    * test
    
    * test
    
    * test header
    
    * adding dist
    
    * adding dist
    
    * adding dist
    
    * vite.config updated
    
    * test
    
    * adding dist subtree
    
    * adding dist subtree
    
    * test
    
    * test deploy
    
    * testing changes
    
    * testing to fetch api endpoints from backend
    
    * test actions
    
    * test
    
    * test
    
    * test again
    
    * test test
    
    * comment out coverage
    
    * added tests to test the test
    
    * second test
    
    * ignore coverage
    
    * comment out threshholds
    
    * test 3
    
    * test 4
    
    * test 5
    
    * test
    
    * test
    
    * github actions
    
    ---------
    
    Co-authored-by: Josh Hurley <53581330+jrhurleycode@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Steve Her <122183092+Sth84478@users.noreply.github.com>
    
    * updated yml files
    
    * adding registration components
    
    * adding registration page
    
    * registration form done.
    
    * registration form validation done
    
    * added error messages for validation
    
    * username validation done
    
    * email validation done. waiting to test
    
    * email validation done
    
    ---------
    
    Co-authored-by: Josh Hurley <53581330+jrhurleycode@users.noreply.github.com>

---
+++ src/components/RegisterComponent.vue
@@ -0,0 +1,208 @@
+<template>
+  <div>
+    <div id="register">
+      <h1>Create Account</h1>
+      <form v-on:submit.prevent="register">
+        <div id="fields">
+          <label for="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            v-model="user.username"
+            autofocus
+          />
+          <span class="err-msg" v-if="v$.user.username.$error">
+            {{ v$.user.username.$errors[0].$message }}
+          </span>
+          <span class="err-msg" v-if="throwUsernameError === true">USERNAME EXISTS</span>
+
+          <label for="email">Email</label>
+          <input type="text" id="email" placeholder="Email" v-model="user.email" />
+          <span class="err-msg" v-if="v$.user.email.$error">
+            {{ v$.user.email.$errors[0].$message }}
+          </span>
+          <span class="err-msg" v-if="throwEmailError === true">EMAIL EXISTS</span>
+
+          <label for="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            v-model="user.password.password"
+          />
+          <span class="err-msg" v-if="v$.user.password.password.$error">
+            {{ v$.user.password.password.$errors[0].$message }}</span
+          >
+
+          <label for="confirmPassword">Confirm password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Confirm Password"
+            v-model="user.password.confirm"
+          />
+          <span class="err-msg" v-if="v$.user.password.confirm.$error">
+            passwords do not match</span
+          >
+
+          <div>
+            <button type="submit">Create Account</button>
+          </div>
+        </div>
+        <hr />
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import authService from '@/services/AuthService'
+import UserService from '@/services/users'
+import useValidate from '@vuelidate/core'
+import { required, email, minLength, sameAs } from '@vuelidate/validators'
+export default {
+  data() {
+    return {
+      v$: useValidate(),
+      user: {
+        username: '',
+        email: '',
+        password: {
+          password: '',
+          confirm: ''
+        }
+      },
+      existingUsers: [],
+      throwUsernameError: false,
+      throwEmailError: false
+    }
+  },
+  validations() {
+    return {
+      user: {
+        username: { required, minLength: minLength(3) },
+        email: { required, email },
+        password: {
+          password: { required, minLength: minLength(6) },
+          confirm: { required, sameAs: sameAs(this.user.password.password) }
+        }
+      }
+    }
+  },
+
+  methods: {
+    async checkUsername() {
+      if (this.user.username) {
+        try {
+          await UserService.getUsers().then((resp) => {
+            this.existingUsers = resp.data
+            const usernameExists = this.existingUsers.some(
+              (existingUser) =>
+                existingUser.username.toLowerCase() === this.user.username.toLowerCase()
+            )
+            if (usernameExists) {
+              this.throwUsernameError = true
+              return false
+            } else {
+              this.throwUsernameError = false
+              return true
+            }
+          })
+        } catch (error) {
+          alert(error)
+        }
+      }
+    },
+
+    async checkEmail() {
+      if (this.user.email) {
+        try {
+          await UserService.getUsers().then((resp) => {
+            this.existingUsers = resp.data
+            const emailExists = this.existingUsers.some(
+              (existingUser) => existingUser.email.toLowerCase() === this.user.email.toLowerCase()
+            )
+            if (emailExists) {
+              this.throwEmailError = true
+              return false
+            } else {
+              this.throwEmailError = false
+              return true
+            }
+          })
+        } catch (error) {
+          alert(error)
+        }
+      }
+    },
+
+    async register() {
+      await this.checkUsername()
+      await this.checkEmail()
+
+      this.v$.$validate()
+      if (
+        !this.v$.$pending &&
+        !this.v$.$error &&
+        this.throwUsernameError === false &&
+        this.throwEmailError === false
+      ) {
+        alert('valid username')
+        // authService
+        //   .register(this.user)
+        //   .then((response) => {
+        //     if (response.status === 201) {
+        //       this.success('Thank you for registering, please sign in.')
+        //       this.$router.push({
+        //         path: '/login'
+        //       })
+        //     }
+        //   })
+        //   .catch((error) => {
+        //     const response = error.response
+        //     if (!response) {
+        //       this.error(error)
+        //     } else if (response.status === 400) {
+        //       if (response.data.errors) {
+        //         // Show the validation errors
+        //         let msg = 'Validation error: '
+        //         for (let err of response.data.errors) {
+        //           msg += `'${err.field}':${err.defaultMessage}. `
+        //         }
+        //         this.error(msg)
+        //       } else {
+        //         this.error(response.data.message)
+        //       }
+        //     } else {
+        //       this.error(response.data.message)
+        //     }
+        //   })
+      } else {
+        alert('Please review your information')
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid;
+  border-radius: 0.375rem;
+  background-color: #ffffff;
+}
+
+#fields {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.err-msg {
+  color: red;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import UsersView from '@/views/UsersView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,14 @@ const router = createRouter({
       path: '/users',
       name: 'users',
       component: UsersView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+      meta: {
+        requiresAuth: false
+      }
     }
   ]
 })

9f1af15b888a1c6e4231760fe5ae0365a19bd6b8
Steve Her <122183092+Sth84478@users.noreply.github.com>
2024-01-20 01:08:59 -0600

    registration tests in progress

--- src/components/RegisterComponent.spec.js
+++ src/components/RegisterComponent.spec.js
@@ -1,82 +1,205 @@
-import { describe, expect, it } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { describe, expect, it, vi } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
 import RegisterComponent from '../components/RegisterComponent.vue'
+import UserService from '@/services/users'
+import AuthService from '@/services/AuthService'
 
 describe('RegisterComponent', () => {
-  const wrapper = mount(RegisterComponent)
+  it('renders the header text', () => {
+    const wrapper = shallowMount(RegisterComponent)
 
-  it('displays inputs correctly', () => {
-    const usernameInput = wrapper.vm.$el.querySelector('input#username')
-    const emailInput = wrapper.vm.$el.querySelector('input#email')
-    const passwordInput = wrapper.vm.$el.querySelector('input#password')
-    const confirmPasswordInput = wrapper.vm.$el.querySelector('input#confirmPassword')
-    const submitButton = wrapper.vm.$el.querySelector('button[type="submit"]')
+    const createAcc = wrapper.get('h2')
+    const loginMsg = wrapper.get('.login-msg')
 
-    expect(usernameInput).not.toBeNull()
-
-    expect(usernameInput).not.toBeNull()
-    expect(usernameInput.placeholder).toBe('Username')
-
-    expect(emailInput).not.toBeNull()
-    expect(emailInput.placeholder).toBe('Email')
-
-    expect(passwordInput).not.toBeNull()
-    expect(passwordInput.placeholder).toBe('Password')
-
-    expect(confirmPasswordInput).not.toBeNull()
-    expect(confirmPasswordInput.placeholder).toBe('Confirm Password')
-
-    expect(submitButton).not.toBeNull()
+    expect(createAcc.text()).toEqual('Create Account')
+    expect(loginMsg.text()).toContain('Already have an account? Login here')
   })
 
-  it('Sets validation errors to true with invalid input to true', async () => {
+  it('renders the username input field', () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    const usernameInput = wrapper.get('#username-input')
+
+    expect(usernameInput.find('label').text()).toEqual('Username')
+    expect(usernameInput.find('InputText').exists()).toBeTruthy
+  })
+
+  it('renders the email input field', () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    const emailInput = wrapper.get('#email-input')
+
+    expect(emailInput.find('label').text()).toEqual('Email')
+    expect(emailInput.find('InputText').exists()).toBeTruthy
+  })
+
+  it('renders the password input field', () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    const passwordInput = wrapper.get('#password-input')
+
+    expect(passwordInput.find('label').text()).toEqual('Password')
+    expect(passwordInput.find('Password').exists()).toBeTruthy
+  })
+
+  it('renders the confirmPassword input field', () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    const confirmPasswordInput = wrapper.get('#confirmPassword-input')
+
+    expect(confirmPasswordInput.find('label').text()).toEqual('Confirm password')
+    expect(confirmPasswordInput.find('Password').exists()).toBeTruthy
+  })
+
+  it('renders the register button', () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    const registerButton = wrapper.get('#register-btn')
+
+    expect(registerButton.exists()).toBeTruthy()
+    expect(registerButton.find('PrimeButton').exists()).toBeTruthy
+  })
+})
+
+describe('RegisterComponent', () => {
+  it('should have initial form validation', async () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    // Set data using setData and wait for the next tick
     await wrapper.setData({
       user: {
-        username: 'spider_bug',
-        email: 'email',
-        password: '12345'
+        username: 'validUsername',
+        email: 'valid@email.com',
+        password: 'ValidPassword123'
       },
-      confirmPassword: '123456'
+      confirmPassword: 'ValidPassword123'
     })
 
-    await wrapper.find('form').trigger('submit.prevent')
+    // Wait for the next tick to ensure data is updated
     await wrapper.vm.$nextTick()
 
-    const usernameError = wrapper.vm.v$.user.username.$error
-    const emailError = wrapper.vm.v$.user.email.$error
-    const passwordError = wrapper.vm.v$.user.password.$error
-    const confirmError = wrapper.vm.v$.confirmPassword.$error
-
-    expect(usernameError).toBeTruthy()
-
-    expect(emailError).toBeTruthy()
-
-    expect(passwordError).toBeTruthy()
-
-    expect(confirmError).toBeTruthy()
+    // Assert that the form becomes valid
+    expect(wrapper.vm.v$.user.username.$error).toBe(false)
+    expect(wrapper.vm.v$.user.email.$error).toBe(false)
+    expect(wrapper.vm.v$.user.password.$error).toBe(false)
+    expect(wrapper.vm.v$.confirmPassword.$error).toBe(false)
   })
 
-  // it('Displays validation error messages with invalid input', async () => {
-  //   await wrapper.setData({
-  //     user: {
-  //       username: 'spider_bug',
-  //       email: 'spider_bug@example.com',
-  //       password: '$2a$10$SpiderBugPassword'
-  //     },
-  //     confirmPassword: 'password'
-  //   })
+  it('should handle form submission', async () => {
+    const wrapper = shallowMount(RegisterComponent)
 
-  //   await wrapper.find('form').trigger('submit.prevent')
-  //   await wrapper.vm.$nextTick()
+    // Mock the form submission
+    wrapper.vm.register = () => {} // Replace with your actual register method
 
-  //   const usernameError = wrapper.vm.$el.querySelector('span#username-err')
-  //   const emailError = wrapper.vm.$el.querySelector('span#email-err')
-  //   const passwordError = wrapper.vm.$el.querySelector('span#password-err')
-  //   const confirmError = wrapper.vm.$el.querySelector('span#confirmPassword-err')
+    // Simulate user input to fill the form
+    wrapper.setData({
+      user: {
+        username: 'validUsername',
+        email: 'valid@email.com',
+        password: 'ValidPassword123'
+      },
+      confirmPassword: 'ValidPassword123'
+    })
 
-  //   expect(usernameError.textContent).toBe('USERNAME EXISTS')
-  //   expect(emailError.textContent).toBe('EMAIL EXISTS')
-  //   expect(passwordError.textContent).toBe('This field should be at least 6 characters long')
-  //   expect(confirmError.textContent).toBe('passwords do not match')
-  // })
+    // Trigger form submission
+    await wrapper.find('form').trigger('submit.prevent')
+
+    // Assert that the register method is called
+    expect(wrapper.vm.register).toHaveBeenCalledOnce
+
+    // Assert that the Toast component is rendered on successful registration
+    // Note: Modify this assertion based on your actual implementation
+    expect(wrapper.findComponent({ name: 'Toast' }).exists()).toBe(true)
+  })
+
+  it('checks the username and sets error if it exists', async () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    // Mock the UserService.getUsers method to return existing users
+    UserService.getUsers = async () => ({ data: [{ username: 'existingUser' }] })
+
+    wrapper.vm.$toast = {
+      add: () => {} // Mock the add method
+    }
+    // Set a valid username
+    await wrapper.setData({ user: { username: 'existingUser' } })
+
+    // Call the checkUsername method
+    await wrapper.vm.checkUsername()
+
+    // Assert that the error is set and toast message is displayed
+    expect(wrapper.vm.throwUsernameError).toBeTruthy
+    // Add more specific assertions if needed
+  })
+
+  it('checks the email and sets error if it exists', async () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    // Mock the UserService.getUsers method to return existing users
+    UserService.getUsers = async () => ({ data: [{ email: 'existing@email.com' }] })
+
+    wrapper.vm.$toast = {
+      add: () => {} // Mock the add method
+    }
+
+    // Set a valid email
+    await wrapper.setData({ user: { email: 'existing@email.com' } })
+
+    // Call the checkEmail method
+    await wrapper.vm.checkEmail()
+
+    // Assert that the error is set and toast message is displayed
+    expect(wrapper.vm.throwEmailError).toBeTruthy
+    // Add more specific assertions if needed
+  })
+
+  it('checks if passwords match', async () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    wrapper.vm.$toast = {
+      add: () => {} // Mock the add method
+    }
+
+    await wrapper.setData({ user: { password: 'password' } })
+    await wrapper.setData({ confirmPassword: 'password' })
+
+    await expect(wrapper.vm.passwordsMatch()).toBeTruthy
+  })
+
+  it('registers successfully', async () => {
+    const wrapper = shallowMount(RegisterComponent)
+
+    const registerMock = vi.fn(() => Promise.resolve({ status: 201 }))
+
+    wrapper.vm.$authService.register = registerMock
+
+    wrapper.vm.checkUsername = async () => true
+    wrapper.vm.checkEmail = async () => true
+    wrapper.vm.v$.$pending = false
+    wrapper.vm.v$.$error = false
+    wrapper.vm.throwUsernameError = false
+    wrapper.vm.throwEmailError = false
+    wrapper.vm.passwordsMatch = true
+
+    // Set valid user data
+    await wrapper.setData({
+      user: {
+        username: 'validUsername',
+        email: 'valid@email.com',
+        password: 'ValidPassword123'
+      },
+      confirmPassword: 'ValidPassword123'
+    })
+
+    // Call the register method
+    await wrapper.vm.register()
+
+    // Expectation
+    expect(registerMock).toHaveBeenCalled()
+    expect(registerMock).toHaveBeenCalledWith({
+      username: 'validUsername',
+      email: 'valid@email.com',
+      password: 'ValidPassword123'
+    })
+  })
 })
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true
+  },
   base: '/bug-wars-Frontend/',
   plugins: [vue()],
   resolve: {

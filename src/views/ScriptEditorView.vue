30cfda87e9577742491cbb07f77311db14b583c3
Steve Her <122183092+Sth84478@users.noreply.github.com>
2024-03-10 14:55:46 -0500

    fixed bugwars logo path

--- src/views/ScriptEditorView.vue
+++ src/views/ScriptEditorView.vue
@@ -1,30 +1,28 @@
 <template>
-
-<!-- Add logo -->
-<div id="logo">
-  <img src="C:\Users\james\workspace\side-projects\crusader-games\bug-wars\bug-wars-Frontend\src\assets\bugwars-logo.png" alt="logo">
-</div>
+  <!-- Add logo -->
+  <div id="logo">
+    <img src="..\assets\bugwars-logo.png" alt="logo" />
+  </div>
   <div id="script-editor-window">
-    <script-editor-component/>
+    <script-editor-component />
   </div>
 </template>
 
 <script>
-import ScriptEditorComponent from '../components/ScriptEditorComponent.vue';
+import ScriptEditorComponent from '../components/ScriptEditorComponent.vue'
 
 export default {
-components: { ScriptEditorComponent }
+  components: { ScriptEditorComponent }
 }
 </script>
 
 <style scoped>
-#logo{
+#logo {
   display: flex;
   justify-content: center;
 }
 
-#script-editor-window{
+#script-editor-window {
   height: 100%;
-  
 }
 </style>

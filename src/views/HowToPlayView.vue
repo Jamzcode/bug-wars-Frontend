b16101c7588a2b78ee1df78515bc3db511cc2c93
Jamzcode <129902520+Jamzcode@users.noreply.github.com>
2024-03-21 13:18:02 -0500

    initial commit, howToPlayView and howToPlayComponent components added, link added to index.js router

---
+++ src/views/HowToPlayView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HowToPlayComponentVue/>
+  </div>
+</template>
+
+<script>
+import HowToPlayComponentVue from '@/components/HowToPlayComponent.vue'
+export default {
+components: {HowToPlayComponentVue},
+}
+</script>
+
+<style scoped>
+
+</style>
